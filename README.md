6dd2d7ac49de78d839a5ac60a466d97e78959381
NikolaMandic <mn080202@gmail.com>
2014-09-17 21:10:47 +0000

    little wrapper

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
 ExAerospike
 ===========
+this is to be updated. for now ->
 
-** TODO: Add description **
+this is light wrapper around aerospike erlang driver      
+      
+usage       
+
+connect         
+         db=ExAerospike         
+         con = db.connect        
+	 con = db.connect 'ip','port'         
+         db.close conn       
+
+options      
+         con = db.namespace con, 'namespace'         
+         con = db.set con, 'aerospikeSetName'         
+
+read     
+         db.get con, 'key','5'         
+         db.get con, 'key'         
+
+write     
+         db.write con, 'key', 'bin', 'value'         
+         db.write con, 'key',['1': '2','3': '4','5': '6']         
+
+del     
+         db.delete con, 'key'         
+         
---
+++ lib/ex_aerospike.ex
@@ -0,0 +1,53 @@
+defmodule ExAerospike do
+  use GenServer
+  defstruct conn: 0,
+            namespace: 'test',
+            set: 'myset',
+            key: "mykey",
+            record: ["mybin": "myvalue"],
+            bin: "mybin",
+            value: "value",
+            records: []
+  def connect(ip \\ '127.0.0.1',port \\ 6000)do
+    {:ok, connectId} = :aerospike.connect(ip, port)
+    %ExAerospike{conn: connectId}
+  end
+  def close(conn)do
+    a=conn
+    :aerospike.shutdown a.conn
+  end
+  def namespace(conn,namespace)do
+    %{conn|namespace: namespace}
+  end
+  def set(conn,set)do
+    %{conn|set: set}
+  end
+  
+  def write(conn,key,records)do
+    a=conn
+    writeResult = :aerospike.put(a.conn, 
+                                 a.namespace, 
+                                 a.set, 
+                                 key, 
+                                 records, 
+                                 0)
+  end
+  def write(conn,key,column,val)do
+    a=conn
+    writeResult = :aerospike.put(a.conn, a.namespace, a.set, key, [{column,val}], 0)
+  end
+  def get(conn,key,column)do
+    a = conn 
+    rr = :aerospike.get(a.conn, a.namespace, a.set, key, [column], 0)
+    [{_,v}]=rr
+    v
+  end
+  def get(conn,key)do
+    a = conn
+    rr = :aerospike.getAll(a.conn, a.namespace, a.set, key, 0)
+    rr
+  end
+  def delete(conn,key)do
+    writeResult = :aerospike.delete(conn.conn, conn.namespace, conn.set, key, 0)
+  end
+end
